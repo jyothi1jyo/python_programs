0670b646869c9b0e7aaa3154d0d995f35d77f726
Jyothi Reddy Bhavanam <31843963+JyothiReddy1997@users.noreply.github.com>
2020-04-25 18:07:22 -0700

    moon
    weight on moon

---
+++ moon_weight.py
@@ -0,0 +1,13 @@
+"""
+File: moon_weight.py
+--------------------
+Add your comments here.
+"""
+
+
+def main():
+    weight = int(input("Enter your weight on Earth: "))
+    print("Your weight on moon is :"+str(0.165*weight))
+
+if __name__ == '__main__':
+    main()
