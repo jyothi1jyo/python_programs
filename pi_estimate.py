8cd5d30d8ff2ddd1c4dac5bbf7564d6030f60fdb
Jyothi Reddy Bhavanam <31843963+JyothiReddy1997@users.noreply.github.com>
2020-04-30 21:28:13 -0700

    error rectified

--- pi_estimate.py
+++ pi_estimate.py
@@ -8,7 +8,7 @@ RADIUS = 1.0
 
 def main():
     print("There are numerous ways to estimate the value of π: while advanced algorithms have computed up to 50 trillion digits of π (as of January 2020), there is also an extremely inefficient yet utmost astonishing method based on bouncing billiards."
-            "A simple probabilistic approach is the so-called Monte Carlo method. As shown below, we have a circle with radius 1, enclosed by a 2 by 2 square. The area ratio of circle to square is π/4. We can then throw a pebble many times, which lands uniformly inside the square. The probability of landing inside the circle is hence π/4.""
+            "A simple probabilistic approach is the so-called Monte Carlo method. As shown below, we have a circle with radius 1, enclosed by a 2 by 2 square. The area ratio of circle to square is π/4. We can then throw a pebble many times, which lands uniformly inside the square. The probability of landing inside the circle is hence π/4."
             "Your task is to write a program that asks a user for the number of throws (n), simulate pebble throwing, and lastly compute the number of times (m) the pebble lands inside the circle. You can then estimate π as 4m/n. As you expect, the estimated value becomes closer to the true value as the number of throws increases.")
     while True:
         num_trials = input('Number of trials (press Enter to quit): ')
