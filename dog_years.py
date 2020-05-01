1369b88fd14493fadae72a33047cff086d14227e
Jyothi Reddy Bhavanam <31843963+JyothiReddy1997@users.noreply.github.com>
2020-04-30 18:57:08 -0700

    calculate dog years

---
+++ dog_years.py
@@ -0,0 +1,7 @@
+
+print("Everyone knows that our furry friends age at a different rate than humans. I am writing a program that"
+"asks the user for a human age (expressed as a whole number) and prints the equivalent dog age"
+"using the fact that there are seven dog years per human year.")
+
+age = int(input("Enter an age in human years: "))
+print("The age in dog years is "+str(age*7))
